ee2c3510596f264a527271ed8fdd954d55820467
Satyam Mishra <satyamgusmishra@gmail.com>
2019-01-17 17:12:52 +0530

    Update spam.js

--- gui/linkers/spam.js
+++ gui/linkers/spam.js
@@ -1,1 +1,19 @@
 
+function check_spam() {
+  var python = require("python-shell")
+  var path = require("path")
+
+  var text = document.getElementById("text").value
+  document.getElementById("text").value = "";
+
+  var options = {
+    scriptPath : path.join(__dirname, '/../engine/'),
+    args : [text]
+  }
+
+  var result = new python('detector.py', options);
+
+  result.on('message', function(message) {
+    swal(message);
+  })
+}
